f5478c37e19a8f24cb3fdeec6605ff0784e8ebf2
Jacob <jhowell45@googlemail.com>
2018-12-06 22:42:31 +0000

    calling function.

--- 2018/run.py
+++ 2018/run.py
@@ -1,6 +1,7 @@
-from day_1.puzzle_1 import calculate_frequence
-from day_1.puzzle_2 import find_repeat_frequency
-from shared_functions import banner, generate_file_data
+from code.day_1.puzzle_1 import calculate_frequence
+from code.day_1.puzzle_2 import find_repeat_frequency
+from code.day_6.puzzle_1 import get_largest_distance
+from code.shared_functions import banner, generate_file_data
 
 
 @banner("Day 1, Puzzle 1")
@@ -15,9 +16,16 @@ def run_day_1_puzzle_2():
     print(f"Final Frequency: {find_repeat_frequency(data)}")
 
 
+@banner("Day 6, Puzzle 1")
+def run_day_6_puzzle_1():
+    data = generate_file_data("./code/day_6/puzzle_data.txt")
+    print(f"Final Frequency: {get_largest_distance(data)}")
+
+
 def main():
     run_day_1_puzzle_1()
     run_day_1_puzzle_2()
+    run_day_6_puzzle_1()
 
 
 if __name__ == "__main__":
