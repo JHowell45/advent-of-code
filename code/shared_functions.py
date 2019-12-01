c268a4e6469466223c902bc089e78f07a96deb22
JHowell45 <jhowell45@googlemail.com>
2019-12-01 19:09:11 +0000

    Deleting old code and restarting

--- code/shared_functions.py
+++ code/shared_functions.py
@@ -1,69 +1,41 @@
 """Use the functions in this file in all of the other src files.
 
-This file contains the functions that will be used across all of the challenges
-and src.
+This file contains the functions that will be used across all of the challenges and
+for every year.
 """
 from os.path import abspath
 
 
-def generate_file_data(filepath):
+def generate_file_data(file_path: str):
     """Use this function to return the data a line at a time.
 
     This function is used for returning the data from the text file one line at
     a time, the data is being returned as generator.
 
-    :param filepath: the path to the data file to use.
-    :type filepath: str
+    :param file_path: the path to the data file to use.
     """
-    with open(abspath(filepath), "r") as f:
+    with open(abspath(file_path), "r") as f:
         for line in f:
             yield line
 
 
-def banner(puzzle_title):
+def banner(puzzle_title: str):
     """Use this function as a banner decorator for wrapping the puzzle answers.
 
     This function is used for decorating the functions for running the puzzle
     functions to help separate out the results and display the information.
 
     :param puzzle_title: the title for the banner.
-    :type puzzle_title: str
     :return: the results of the wrapped function.
-    :rtype:
     """
     title_length = len(puzzle_title)
     banner_length = 30
 
     def dec(function):
-        """Use this function to add as a decorator for wrapping the function.
-
-        This function is used for adding a wrapping function to the decorated
-        function.
-
-        :param function: the function to wrap.
-        :type function:
-        :return: the results of the wrapped function.
-        :rtype:
-        """
-
         def func(*args, **kwargs):
-            """Use this function to add a banner to the wrapped function.
-
-            This function is used for wrapping the wrapped function in
-            additional functionality.
-
-            :return: the results of the wrapped function.
-            :rtype:
-            """
-            print(
-                "\n|{1}| {0} |{1}|\n".format(puzzle_title, "-" * banner_length)
-            )
+            print("\n|{1}| {0} |{1}|\n".format(puzzle_title, "-" * banner_length))
             result = function(*args, **kwargs)
-            print(
-                "\n|{}|\n".format(
-                    "-" * ((banner_length * 2) + title_length + 4)
-                )
-            )
+            print("\n|{}|\n".format("-" * ((banner_length * 2) + title_length + 4)))
             return result
 
         return func
