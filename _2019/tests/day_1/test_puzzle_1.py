4a38612305c59007bd00b11c39f3916abcabd253
JHowell45 <jhowell45@googlemail.com>
2019-12-01 19:19:23 +0000

    Created function and tests.

---
+++ _2019/tests/day_1/test_puzzle_1.py
@@ -0,0 +1,9 @@
+"""Use this file for testing the functions for day 1 puzzle 1."""
+import pytest
+
+from _2019.src.day_1.puzzle_1 import calculate_fuel
+
+
+@pytest.mark.parametrize("mass,fuel", [(12, 2), (14, 2), (1969, 654), (100756, 33583)])
+def test_calculate_fuel(mass: int, fuel: int):
+    assert calculate_fuel(mass) == fuel
