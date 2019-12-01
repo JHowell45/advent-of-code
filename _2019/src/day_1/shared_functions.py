fc0bcc34102ad1c57197e0454141a333dd5cbb67
JHowell45 <jhowell45@googlemail.com>
2019-12-01 20:35:02 +0000

    Created shared functions.

---
+++ _2019/src/day_1/shared_functions.py
@@ -0,0 +1,27 @@
+"""Use this file to include shared function for the day 1 puzzles."""
+from os.path import abspath, realpath
+
+
+def calculate_fuel(mass: int) -> int:
+    """Use this function for calculating the fuel requirements for a module.
+
+    This function is used for calculating the fuel requirements for a module based on
+    its mass.
+
+    :param mass: the mass of the module.
+    :return: the calculated fuel.
+    """
+    return int(mass / 3) - 2
+
+
+def get_puzzle_input() -> int:
+    """Use this function to get the puzzle data input in the correct format.
+
+    This function is used for iterating over the puzzle data file and returning the
+    values in the correct format.
+
+    :yield: correctly formatted row of file.
+    """
+    with open(f"{abspath(f'{realpath(__file__)}/../puzzle_data.txt')}") as f:
+        for line in f:
+            yield int(line)
