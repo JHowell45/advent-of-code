49ae328d8d16bdbde5d00292989a594fed72b64e
JHowell45 <jhowell45@googlemail.com>
2019-12-02 21:25:55 +0000

    Creating tests for new class.

---
+++ _2019/tests/day_2/test_puzzle_1.py
@@ -0,0 +1,17 @@
+"""Use this file for testing the functions for day 2 puzzle 1."""
+import pytest
+
+from _2019.src.day_2.puzzle_1 import IntCode
+
+
+@pytest.mark.parametrize(
+    "initial_state,final_state",
+    [
+        ("1,0,0,0,99", "2,0,0,0,99"),
+        ("2,3,0,3,99", "2,3,0,6,99"),
+        ("2,4,4,5,99,0", "2,4,4,5,99,9801"),
+        ("1,1,1,4,99,5,6,0,99", "30,1,1,4,2,5,6,0,99"),
+    ],
+)
+def test_calculate_fuel(initial_state: str, final_state: str):
+    assert IntCode(initial_state).final_state == final_state
